cca307902b38ff96f26699bcbf06dddbd3421816
Rodrigo Santelices <rcsantelices@gmail.com>
2018-02-12 20:55:18 -0800

    added content to html

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+1. User will choose to search for tasting rooms or wineries within a certain radius
+2. the user will be presented with a map, and the top options matching their query
+3. The user can click the map, which will minimieze the top options
+4. the user can click the options which will minimize the map
+5. Upon clicking on an option on the map or the options screen the user will be taken
+to a highlighted page for the option
